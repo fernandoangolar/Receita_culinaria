272db6d3636d6913fc72ae3a2ea878b6e39e4834
Fernando Angolar <fernandoangolar581@gmail.com>
2023-11-14 08:28:34 +0100

    Adicionando a tela de menu da aplicação

---
+++ app/src/main/java/com/evandre/ao/receitaculinariaangolar/activity/MenuApp.java
@@ -0,0 +1,16 @@
+package com.evandre.ao.receitaculinariaangolar.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.evandre.ao.receitaculinariaangolar.R;
+
+public class MenuApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_app);
+    }
+}
